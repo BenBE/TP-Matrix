5f49331fd45b66348ed87f1f97e3150e97fa970e
Benny Baumann <BenBE@geshi.org>
2012-02-14 23:40:57 +0100

    upd: Etwas andere Texte für die Laufschriften

--- string_anim.c
+++ string_anim.c
@@ -33,18 +33,23 @@ void string_anim (char id)
 
     switch (id) {
         case 0:
-            string_doanim ("CHAOS IN KIEL?", 250, display_color_from_rgb (160, 160, 255) );
+            string_doanim ("CHAOS in Kiel?", 250, display_color_from_rgb (160, 160, 255) );
             break;
         case 1:
-            string_doanim ("TOPPOINT E.V.", 250, display_color_from_rgb (127, 0, 255) );
+            string_doanim ("Toppoint e.V.", 250, display_color_from_rgb (127, 0, 255) );
             break;
         case 2:
-            string_doanim ("Y U NO FRICKL?", 250, display_color_from_rgb (255, 0, 0) );
+            string_doanim ("Y U NO FRICKL?", 250, display_color_from_rgb (255, 255, 0) );
+            break;
+        case 3:
+            string_doanim ("Diese Laufschrift ist in Ihrem Land nicht verf\206gbar. Das tut uns (nicht) leid!", 250, display_color_from_rgb (255, 255, 255) );
+            string_doanim ("Stoppt Zensursula, SOPA, PIPA, ACTA, ...!", 500, display_color_from_rgb (255, 0, 0) );
+            string_doanim ("F\206r m\206ndigen Umgang mit Indernet und Netzneutralit\204t!", 500, display_color_from_rgb (0, 255, 0) );
             break;
         case 42:
             string_doanim ("THE ANSWER TO LIFE, THE UNIVERSE AND EVERYTHING?        42!", 400, display_color_from_rgb (255, 255, 255) );
             break;
         default:
-            string_doanim ("FRICKL", 500, display_color_from_rgb (0, 255, 0) );
+            string_doanim ("Bastelst Du noch, oder FRICKLst Du schon?", 500, display_color_from_rgb (0, 255, 0) );
     }
 }
