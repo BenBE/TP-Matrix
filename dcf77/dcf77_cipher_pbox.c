d64a304837ee737fef9e7472f4297cc66b3d9f29
Benny Baumann <BenBE@geshi.org>
2012-07-29 17:55:22 +0200

    add: Implement PBOX function for DCF77 weather decoding

--- dcf77/dcf77_cipher_pbox.c
+++ dcf77/dcf77_cipher_pbox.c
@@ -2,5 +2,14 @@
 #include <dcf77_keys.h>
 
 void dcf77_cipher_pbox(dcf77_cipherblock_t *data, uint32_t key) {
+    uint32_t val4 = 0;
+    register uint32_t tmp = 0x00000001;
 
+    for(volatile uint8_t i = 0; i < sizeof(dcf77_key_pbox); i++) {
+        if((val4 & (1 << dcf77_cipherdata_read(&dcf77_key_pbox[i]))) != 0) {
+            key |= tmp;
+        }
+
+        tmp <<= 1;
+    }
 }
