2bb8c2ca124e7f501da74f859d67e54be858eb71
Benny Baumann <BenBE@geshi.org>
2012-07-29 23:33:55 +0200

    add: Add Loop Detection and gradual fading when running into a dead end

--- game_of_life.c
+++ game_of_life.c
@@ -69,9 +69,27 @@ static void do_game_of_life()
     }
 }
 
+#define CRC32POLY 0xEDB88320 /* CRC-32 Polynom, umgekehrte Bitfolge */
+static uint32_t get_state_checksum(void) {
+    uint32_t crc32 = ~0;
+
+    const color_t *pxl = &display[1][0][0];
+    for(int offset = 0; offset < DISPLAY_WIDTH * DISPLAY_HEIGHT; offset++) {
+        if ((crc32 & 1) != (!!pxl[offset])) {
+            crc32 = (crc32 >> 1) ^ CRC32POLY;
+        } else {
+            crc32 >>= 1;
+        }
+    }
+
+    return ~crc32;
+}
+
 void game_of_life()
 {
     display_buffer_write_set (0);
+    display_clear_black();
+    display_buffer_write_set (2);
     display_clear_black();
     display_buffer_active_set (0);
 
@@ -81,9 +99,40 @@ void game_of_life()
     for (int i = 0; i < 256; i++) {
         do_game_of_life();
 
-        if (!memcmp (display[0], display[1], DISPLAY_WIDTH * DISPLAY_HEIGHT) ) {
-            init_random();
+//        if (!memcmp (display[0], display[1], DISPLAY_WIDTH * DISPLAY_HEIGHT) ) {
+//            init_random();
+//        }
+
+        //Check for loops
+        uint32_t curstate = get_state_checksum();
+        uint32_t * const recent = (uint32_t *)(&display[2][0][0]);
+        int maxRecent = (DISPLAY_WIDTH * DISPLAY_HEIGHT) / sizeof(uint32_t);
+        for(int offset = 0; offset < maxRecent - 1; offset++) {
+            if( (recent[offset] == curstate) && (recent[0] == recent[offset+1]) ) {
+
+                //Gradually fade out ...
+                uint32_t sum;
+                do {
+                    sum = 0;
+                    color_t * const cd = &display[1][0][0];
+                    for(int offset = 0; offset < DISPLAY_WIDTH * DISPLAY_HEIGHT; offset++) {
+                        cd[offset] = !cd[offset] ? 0 :
+                            ((((cd[offset] - 1) & 006) << 5) + ((cd[offset] - 1) & 007) * 011);
+                        sum += !!cd[offset];
+                    }
+
+                    display_buffer_copy (1, 0);
+                    delay_ms(50);
+                } while (sum);
+
+                init_random();
+                break;
+            }
         }
+        for(int offset = maxRecent - 1; offset > 0; offset--) {
+            recent[offset] = recent[offset - 1];
+        }
+        recent[0] = curstate;
 
         display_buffer_copy (1, 0);
         delay_ms (50);
