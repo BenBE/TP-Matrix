d5d0e8f302f2ab227bef1bc380d4b9b7dcb8a940
Daniel Ehlers <danielehlers@mindeye.net>
2012-09-15 18:18:57 +0200

    Revert "Fix font problem."
    This reverts commit 6c39497279dd8e33bea45892438c141096805f23.

--- uTPLib/font_char_print.c
+++ uTPLib/font_char_print.c
@@ -35,10 +35,8 @@ void font_char_print(coord_t x, coord_t y, color_t color, char c)
         {
             break;
         }
-        
-        // This is maybe due to a pgm_read_byte behaviour change
-        // uint8_t pxl = pgm_read_byte(&font_data[dx]);
-        uint8_t pxl = pgm_read_byte(*font_data++);
+
+        uint8_t pxl = pgm_read_byte(&font_data[dx]);
         for(coord_t dy = 0; dy < FONT_HEIGHT; dy++)
         {
             if(pxl & 1)
