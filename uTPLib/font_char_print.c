6c39497279dd8e33bea45892438c141096805f23
Daniel Ehlers <danielehlers@mindeye.net>
2012-09-15 12:06:41 +0200

    Fix font problem.
    This is possible a pgm access problem
    or this only works on the at_mega due to
    architecture stuff.

--- uTPLib/font_char_print.c
+++ uTPLib/font_char_print.c
@@ -35,8 +35,10 @@ void font_char_print(coord_t x, coord_t y, color_t color, char c)
         {
             break;
         }
-
-        uint8_t pxl = pgm_read_byte(&font_data[dx]);
+        
+        // This is maybe due to a pgm_read_byte behaviour change
+        // uint8_t pxl = pgm_read_byte(&font_data[dx]);
+        uint8_t pxl = pgm_read_byte(*font_data++);
         for(coord_t dy = 0; dy < FONT_HEIGHT; dy++)
         {
             if(pxl & 1)
