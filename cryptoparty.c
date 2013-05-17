357cfaccb8e4e1f495e05d87b552d9664591e795
Benny Baumann <BenBE@geshi.org>
2013-05-17 23:50:48 +0200

    add: Use BGCOLOR from a compile-time constant

--- cryptoparty.c
+++ cryptoparty.c
@@ -38,17 +38,18 @@ const uint8_t PROGMEM keyindexV2 [8] = {
     4,5,6,7,7,6,5,4
 };
 
+#define BGCOLOR 0
+
 void cryptoparty() {
     display_buffer_write_set(0);
-    display_clear_color(255);
+    display_clear_color(BGCOLOR);
     display_buffer_active_set(0);
     display_buffer_write_set(1);
-    display_clear_color(255);
-
+    display_clear_color(BGCOLOR);
 
     for( int i = 0; i<5; i++){
       for( uint8_t frame = 0; frame<sizeof(keyindexV1);frame++){
-            display_clear_color(255);
+        display_clear_color(BGCOLOR);
         display_sprite_put_P(10, 0, 19, 4, display_color_from_rgb(0xd9, 0x00, 0xff),crypto); //crypt
         display_sprite_put_P(18, 4, 19, 4, display_color_from_rgb( 0xd9, 0x00, 0xff),party);  //party
         display_sprite_put_P(10, 4, 7, 4, display_color_from_rgb( 255, 0, 152 ), cryptokey[pgm_read_byte(&keyindexH2[frame])]);
